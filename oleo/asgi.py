245306be90ed26a7628cce47143129c8e9e880c6
abbauz <105697297+abbauz@users.noreply.github.com>
2023-03-04 00:12:07 +0600

    production changes

--- oleo/asgi.py
+++ oleo/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oleo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oleo.production')
 
 application = get_asgi_application()
